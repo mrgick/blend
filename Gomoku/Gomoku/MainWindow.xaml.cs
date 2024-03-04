3d302aa1bace675ec1f0e512362fe0e01db2b8ff
mrgick <mrgickcool@gmail.com>
2024-03-04 23:02:06 +0300

    Gomuku. Add animations to ellipse.

--- Gomoku/Gomoku/MainWindow.xaml.cs
+++ Gomoku/Gomoku/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace Gomoku
         int compLV = 1;
         const int nRows = 10, nCols = 10;
         double baseHeight = 65.0, baseWidth = 65.0;
+        private static Random random = new Random();
         GameLogic gameLogic;
         Computer computer;
         Button[,] buttonBoard;
@@ -84,33 +85,96 @@ namespace Gomoku
         private void DrawEllipse(int r, int c)
         {
             Ellipse ellipse = new Ellipse();
-            ellipse.Stroke = Brushes.Red;
-            ellipse.StrokeThickness = 5;
+            ellipse.Stroke = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E36687"));
+            ellipse.StrokeThickness = 7;
             ellipse.Width = baseWidth - 5;
             ellipse.Height = baseHeight - 5;
             Grid.SetRow(ellipse, r);
             Grid.SetColumn(ellipse, c);
             fieldGrid.Children.Add(ellipse);
+            int rand = random.Next(1, 5);
+            if (rand == 1)
+            {
+                DoubleAnimation anim = new DoubleAnimation();
+                if (random.Next(0,2) == 0)
+                {
+                    anim.From = random.Next(0, Convert.ToInt32(baseWidth) - 10);
+                    anim.To = baseWidth - 5;
+                } else
+                {
+                    anim.From = random.Next(Convert.ToInt32(baseWidth), Convert.ToInt32(baseWidth)+10);
+                    anim.To = baseWidth - 5;
+                }
+                anim.Duration = TimeSpan.FromSeconds(0.3);
+                ellipse.BeginAnimation(Ellipse.WidthProperty, anim);
+                ellipse.BeginAnimation(Ellipse.HeightProperty, anim);
+            } else if (rand == 2)
+            {
+                ColorAnimation anim = new ColorAnimation();
+                String[] colors = { "#0034E6", "#E6A800", "#00E61E", "#663341", "#66C4E3" };
+                anim.From = (Color)ColorConverter.ConvertFromString(colors[random.Next(0, colors.Length)]);
+                anim.To = (Color)ColorConverter.ConvertFromString("#E36687");
+                anim.Duration = TimeSpan.FromSeconds(0.4);
+                ellipse.Stroke.BeginAnimation(SolidColorBrush.ColorProperty, anim);
+            } else if (rand == 3)
+            {
+                DoubleAnimation anim = new DoubleAnimation();
+                if (random.Next(0, 2) == 0)
+                {
+                    anim.From = random.Next(0, 6);
+                    anim.To = 7;
+                }
+                else
+                {
+                    anim.From = random.Next(8, 20);
+                    anim.To = 7;
+                }
+                anim.Duration = TimeSpan.FromSeconds(0.3);
+                ellipse.BeginAnimation(Ellipse.StrokeThicknessProperty, anim);
+            } else if (rand == 4) 
+            {
+                ThicknessAnimation anim = new ThicknessAnimation();
+                int margin = -random.Next(10, Convert.ToInt32(baseWidth)+1);
+                int rnd2 = random.Next(0, 4);
+                if (rnd2 == 0)
+                {
+                    anim.From = new Thickness(margin, 0, 0, 0);
+                } else if (rnd2 == 1)
+                {
+                    anim.From = new Thickness(0, margin, 0, 0);
+                }
+                else if (rnd2 == 2)
+                {
+                    anim.From = new Thickness(0, 0, margin, 0);
+                }
+                else if (rnd2 == 3)
+                {
+                    anim.From = new Thickness(0, 0, 0, margin);
+                }
+                anim.To = new Thickness(0, 0, 0, 0);
+                anim.Duration = TimeSpan.FromSeconds(0.3);
+                ellipse.BeginAnimation(Ellipse.MarginProperty, anim);
+            }
         }
 
         private void DrawCross(int r, int c)
         {
             Line line = new Line();
-            line.Stroke = Brushes.Blue;
-            line.StrokeThickness = 5;
-            line.X1 = 0;
-            line.Y1 = 0;
-            line.X2 = baseWidth;
-            line.Y2 = baseHeight;
+            line.Stroke = (SolidColorBrush)(new BrushConverter().ConvertFrom("#66C4E3"));
+            line.StrokeThickness = 7;
+            line.X1 = 5;
+            line.Y1 = 5;
+            line.X2 = baseWidth - 5;
+            line.Y2 = baseHeight - 5;
             Grid.SetRow(line, r);
             Grid.SetColumn(line, c);
             Line line2 = new Line();
-            line2.Stroke = Brushes.Blue;
-            line2.StrokeThickness = 5;
-            line2.X1 = 0;
-            line2.Y1 = baseHeight;
-            line2.X2 = baseWidth;
-            line2.Y2 = 0;
+            line2.Stroke = (SolidColorBrush)(new BrushConverter().ConvertFrom("#66C4E3"));
+            line2.StrokeThickness = 7;
+            line2.X1 = 5;
+            line2.Y1 = baseHeight - 5;
+            line2.X2 = baseWidth - 5;
+            line2.Y2 = 5;
             Grid.SetRow(line2, r);
             Grid.SetColumn(line2, c);
             fieldGrid.Children.Add(line);
@@ -128,11 +192,11 @@ namespace Gomoku
             else if (result == 1)
             {
                 EndMessage.Text = "You win!";
-                EndMessage.Foreground = Brushes.Lime;
+                EndMessage.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#00E676"));
             }
             else if (result == 2) {
                 EndMessage.Text = "You lose!";
-                EndMessage.Foreground = Brushes.Red;
+                EndMessage.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E61B00"));
             }
             
             Storyboard sb = (Storyboard)TryFindResource("Storyboard_TextBlock");
